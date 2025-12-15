d7de8af9be29631bcec47c7d17548e72db7b6cf4
Rayhan Nozawa🍟 <rayhanbaik79@gmail.com>
2025-12-15 14:11:22 +0700

    Add files via upload

--- plugins/ai-flux.js
+++ plugins/ai-flux.js
@@ -15,12 +15,7 @@ export const run = {
 
   try {
     conn.sendChatAction(m.chat, "upload_photo")
-    const result = await fluximg.create(text);
-    if (result && result.imageLink) {
-      await conn.sendButton(m.chat, donateBtn, result.imageLink, 'hasil.jpg', `Hasil Flux:\n\nPrompt: ${text}`, m.msg, env.wm);
-    } else {
-      throw new Error("Gagal membuat gambar. Coba lagi.");
-    }
+    await conn.sendButton(m.chat, donateBtn, `${apiUrl}/flux?prompt=${text}`, 'hasil.jpg', `Hasil Flux:\n\nPrompt: ${text}`, m.msg, env.wm);
   } catch (error) {
     return m.reply("Terjadi kesalahan saat membuat gambar: " + error.message)
   }
@@ -31,29 +26,3 @@ export const run = {
    limit: true,
    location: __filename
 }
-
-const fluximg = {
-  defaultRatio: "2:3",
-
-  create: async (query) => {
-    const config = {
-      headers: {
-        accept: "*/*",
-        authority: "1yjs1yldj7.execute-api.us-east-1.amazonaws.com",
-        "user-agent": "Postify/1.0.0",
-      },
-    };
-
-    try {
-      const response = await axios.get(`https://1yjs1yldj7.execute-api.us-east-1.amazonaws.com/default/ai_image?prompt=${encodeURIComponent(query)}&aspect_ratio=${fluximg.defaultRatio}`,
-        config
-      );
-      return {
-        imageLink: response.data.image_link,
-      };
-    } catch (error) {
-      console.error(error);
-      throw error;
-    }
-  },
-};
