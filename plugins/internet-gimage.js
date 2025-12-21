a303691725198f5e5f31cb22f7c32e279676c43c
Rayhan Nozawa🍟 <rayhanbaik79@gmail.com>
2025-12-21 10:57:36 +0700

    Add files via upload

--- plugins/internet-gimage.js
+++ plugins/internet-gimage.js
@@ -11,67 +11,26 @@ export const run = {
     conn.sendChatAction(m.chat, "upload_photo");
 
     try {
-      const { data } = await axios.get(`https://restapi-v2.simplebot.my.id/search/gimage?q=${text}`);
+      const { data } = await axios.get(`${apiUrl}/gimage?query=${text}`);
 
-      const images = [...new Set(data.result.map(v => v.url))].slice(0, 20);
       const caption = `Result from ${text}`;
-
-      const batches = createBatches(images, 10);
-
-      for (const batch of batches) {
-        const media = [];
-
-        for (let i = 0; i < batch.length; i++) {
-          try {
-            const { buffer, filename } = await getImage(batch[i]);
-
-            media.push({
-              type: 'photo',
-              media: batch[i],
-              caption: i === 0 ? caption : undefined
-            });
-          } catch {
-            // skip image rusak
-          }
-        }
-
-        if (media.length > 1) {
-          await conn.sendMediaGroup(m.chat, media);
-        }
+      
+      const media = [];
+      for (let i = 0; i < data.length; i++) {
+        media.push({
+          type: 'photo',
+          media: data[i],
+          caption: i === 0 ? caption : undefined
+        });
       }
-
+        
+      if (media.length > 1) {
+        await conn.sendMediaGroup(m.chat, media);
+      } else {
+        await conn.sendPhoto(m.chat, media[0].media, { caption });
+      }
     } catch (e) {
       m.reply(e.message);
     }
   }
 };
-
-function createBatches(arr, size = 10) {
-  const out = [];
-  for (let i = 0; i < arr.length; i += size) {
-    out.push(arr.slice(i, i + size));
-  }
-  return out;
-}
-
-async function getImage(url) {
-  const res = await axios.get(url, {
-    responseType: 'arraybuffer',
-    timeout: 15000,
-    headers: {
-      'User-Agent': 'Mozilla/5.0',
-      'Accept': 'image/*'
-    }
-  });
-
-  const type = res.headers['content-type'];
-  if (!type || !type.startsWith('image/')) {
-    throw new Error('Not image');
-  }
-
-  const ext = type.split('/')[1];
-  return {
-    buffer: Buffer.from(res.data),
-    filename: `image.${ext}`
-  };
-}
